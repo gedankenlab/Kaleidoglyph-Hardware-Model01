4f476d8f0f882dc93aa5d42e7128be84eae2df1f
Michael Richters <gedankenexperimenter@gmail.com>
2017-12-09 22:39:03 -0600

    Fixed the other keymap macro translation matrix

--- src/Kaleidoscope-Hardware-Model01.h
+++ src/Kaleidoscope-Hardware-Model01.h
@@ -248,16 +248,21 @@ class Model01 {
     r7c7,  r7c6,  r7c5,  r7c4,  r7c3,  r7c2,  r7c1,  r7c0,		\
   }
 
-#define KEYMAP(                                                                                     \
-  r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6,        r0c9,  r0c10, r0c11, r0c12, r0c13, r0c14, r0c15, \
-  r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6,        r1c9,  r1c10, r1c11, r1c12, r1c13, r1c14, r1c15, \
-  r2c0, r2c1, r2c2, r2c3, r2c4, r2c5,                     r2c10, r2c11, r2c12, r2c13, r2c14, r2c15, \
-  r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r2c6,        r2c9,  r3c10, r3c11, r3c12, r3c13, r3c14, r3c15, \
-              r0c7, r1c7, r2c7, r3c7,                             r3c8,  r2c8,  r1c8, r0c8,         \
-                          r3c6,                                          r3c9)                      \
-  {                                                                                                 \
-    r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6, r0c7, r0c8, r0c9, r0c10, r0c11, r0c12, r0c13, r0c14, r0c15, \
-    r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8, r1c9, r1c10, r1c11, r1c12, r1c13, r1c14, r1c15, \
-    r2c0, r2c1, r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, r2c8, r2c9, r2c10, r2c11, r2c12, r2c13, r2c14, r2c15, \
-    r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, r3c8, r3c9, r3c10, r3c11, r3c12, r3c13, r3c14, r3c15, \
+#define KEYMAP(											\
+  r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6,        r4c1, r4c2, r4c3, r4c4, r4c5, r4c6, r4c7,	\
+  r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6,        r5c1, r5c2, r5c3, r5c4, r5c5, r5c6, r5c7,	\
+  r2c0, r2c1, r2c2, r2c3, r2c4, r2c5,                    r6c2, r6c3, r6c4, r6c5, r6c6, r6c7,	\
+  r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r2c6,        r6c1, r7c2, r7c3, r7c4, r7c5, r7c6, r7c7,	\
+              r0c7, r1c7, r2c7, r3c7,                    r7c0,  r6c0,  r5c0, r4c0,		\
+                          r3c6,                                 r7c1)				\
+  {												\
+    r0c7,  r0c6,  r0c5,  r0c4,  r0c3,  r0c2,  r0c1,  r0c0,					\
+    r1c7,  r1c6,  r1c5,  r1c4,  r1c3,  r1c2,  r1c1,  r1c0,					\
+    r2c7,  r2c6,  r2c5,  r2c4,  r2c3,  r2c2,  r2c1,  r2c0,					\
+    r3c7,  r3c6,  r3c5,  r3c4,  r3c3,  r3c2,  r3c1,  r3c0,					\
+												\
+    r4c7,  r4c6,  r4c5,  r4c4,  r4c3,  r4c2,  r4c1,  r4c0,					\
+    r5c7,  r5c6,  r5c5,  r5c4,  r5c3,  r5c2,  r5c1,  r5c0,					\
+    r6c7,  r6c6,  r6c5,  r6c4,  r6c3,  r6c2,  r6c1,  r6c0,					\
+    r7c7,  r7c6,  r7c5,  r7c4,  r7c3,  r7c2,  r7c1,  r7c0,					\
   }
