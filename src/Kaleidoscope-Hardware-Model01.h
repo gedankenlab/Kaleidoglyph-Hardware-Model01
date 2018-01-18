d3c053e58040046a652a11455297d81852dea259
Michael Richters <gedankenexperimenter@gmail.com>
2018-01-18 12:11:31 -0600

    Moved KeyAddr definition back to Kaleidoscope-Hardware-Model01.h
    This is meant to prevent accidental inclusion of the wrong KeyAddr.h header file because
    of search order ambiguity

--- src/Kaleidoscope-Hardware-Model01.h
+++ src/Kaleidoscope-Hardware-Model01.h
@@ -7,15 +7,24 @@
 #define HARDWARE_IMPLEMENTATION Model01
 #include <KeyboardioScanner.h>
 
-namespace kaleidoscope {
-namespace hardware {
+#define UNKNOWN_KEY_ADDR  kaleidoscope::hardware::Model01::total_keys;
+#define TOTAL_KEYS        kaleidoscope::hardware::Model01::total_keys;
 
-// I think this should actually be a member variable of the hardware object
-constexpr byte TOTAL_KEYS = 64;
+
+namespace kaleidoscope {
+
+// The Model01 has only 64 keys, and we only need one extra value to indicate an invalid
+// key address, so that all fits in one byte.
+typedef byte KeyAddr;
+
+
+namespace hardware {
 
 class Model01 {
  public:
   Model01();
+
+  static constexpr KeyAddr total_keys = 64;
 
   // Not sure what this is for yet
   void syncLeds();
@@ -47,7 +56,7 @@ class Model01 {
   KeyData right_hand_state;
   KeyData previous_left_hand_state;
   KeyData previous_right_hand_state;
-  // like this?
+  // like this: hand_state[left|right][prev|cur]
   KeyData hand_state[2][2];
 
  private:
@@ -57,6 +66,38 @@ class Model01 {
 };
 
 } // namespace hardware {
+
+
+namespace keyaddr {
+
+// Everything past this point should be unnecessary, but could possibly be helpful for
+// plugins that want to affect whole rows or columns. Note that this code defines 8 rows &
+// 8 columns, which is probably not what a user wants on a Model01 (the last four rows are
+// on the right half of the keyboard.
+
+// ROWS = 8
+// COLS = 8
+constexpr KeyAddr ROW_BITS = B00111000;
+constexpr KeyAddr COL_BITS = B00000111;
+
+// Conversion functions to help with back-compat and user addressing. I actually think
+// that these addresses are not great, since the thumb arc should be a separate row, and
+// the palm keys yet another. User addressing should maybe be this 3-tuple: {hand, row,
+// col}, but that could be handled with #defines, or the build scripts.
+constexpr byte row(KeyAddr key_addr) {
+  return (key_addr & ROW_BITS) >> 3;
+}
+
+constexpr byte col(KeyAddr key_addr) {
+  return (key_addr & COL_BITS);
+}
+
+constexpr KeyAddr addr(byte row, byte col) {
+  return ((row << 3) | col);
+}
+
+} // namespace keyaddr {
+
 } // namespace kaleidoscope {
 
 
