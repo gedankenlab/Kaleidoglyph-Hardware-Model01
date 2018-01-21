8b21e014394ecef74f332266840b89caa2fdc8fa
Michael Richters <gedankenexperimenter@gmail.com>
2018-01-20 19:02:43 -0600

    Use alias for kaleidoscope::KeyAddr, et al

--- src/Kaleidoscope-Hardware-Model01.h
+++ src/Kaleidoscope-Hardware-Model01.h
@@ -4,12 +4,10 @@
 
 #include "kaleidoscope/model01/Keyboard.h"
 
-#define KeyAddr model01::KeyAddr
-#define LedAddr model01::LedAddr
-#define Color   model01::Color
-
-// or:
+namespace kaleidoscope {
 
 using KeyAddr = model01::KeyAddr;
 using LedAddr = model01::LedAddr;
 using Color   = model01::Color;
+
+} // namespace kaleidoscope {
