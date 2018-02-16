951bafd88a99b9a7670fae0f82cd9827ee508988
Michael Richters <gedankenexperimenter@gmail.com>
2018-02-16 14:37:17 -0600

    More improvements to Key class
    * add support for different key types, with functions for identifying them
    * add KeyClass enum type for use with switch statements
    * add modifiers definitions for keyboard type

--- src/model01/Key.h
+++ src/model01/Key.h
@@ -23,16 +23,41 @@ union Key {
   struct {
     // AVR is little-endian, which is why this file is here instead of in the core. In the
     // event of an MCU that's big-endian, these two bytes would need to be swapped
-    uint8_t code;  // low bits (keycode)
-    uint8_t meta;  // high bits (flags)
-  };
-  struct {
-    uint8_t keycode;
-    struct {
-      uint8_t modifier_flags : 4, hand : 1, key_type : 3;
-    };
+    uint8_t ldata;  // low bits (keycode)
+    uint8_t hdata;  // high bits (flags)
   };
 
+  // Keyboard key type: 8 bits for keycode, 4 modifier flags, one modifier hand flag, and
+  // three bits for type identification (type is all zeros)
+  struct {
+    byte keycode;
+    byte mods : 4, hand : 1, type : 3;
+  } keyboard;
+
+  // Consumer Control key type: 10 bits for keycode, 6 for type
+  struct {
+    uint16_t keycode : 10, type : 6;
+  } consumer;
+
+  // System Control key type: 8 bits for keycode, 8 for type
+  struct {
+    byte keycode;
+    byte type;
+  } system;
+
+  // Mouse key type: 8 bits for keycode, 8 for type
+  struct {
+    byte keycode;
+    byte type;
+  } mouse;
+
+  // Plugin key type: only two bits for identifying the type (could be just one), and the
+  // rest can be used for identifying plugins, and storing values
+  struct {
+    uint16_t keycode : 14, type : 2;
+  } plugin;
+
+  // Constructors
   Key() = default;
   constexpr Key(uint8_t _code, uint8_t _meta) : code(_code), meta(_meta) {}
   constexpr Key(uint16_t _raw) : raw(_raw) {}
@@ -59,30 +84,29 @@ union Key {
     return new_key;
   }
 
+  static constexpr byte mod_control = B0001;
+  static constexpr byte mod_shift   = B0010;
+  static constexpr byte mod_alt     = B0100;
+  static constexpr byte mod_gui     = B1000;
+
+  static constexpr byte mods_left  = B0;
+  static constexpr byte mods_right = B1;
+
+  // get a modifiers byte that can be passed to the key report directly
+  byte mods() {
+    if (keyboard.type != Key::keyboard_type)
+      return 0;
+    byte modifiers(keyboard.mods);
+    if (keyboard.hand == Key::mods_right)
+      modifiers <<= 4;
+    return modifiers;
+  }
+
   constexpr bool isTransparent() const {
     return (raw == 0xFFFF);
   }
   constexpr bool isBlank() const {
     return (raw == 0x0000);
-  }
-
-  // Comparison operators (integers). I don't think these are really necessary, or even
-  // desirable; we should remember to cast integers appropriately, instead. There's
-  // probably a small benefit in performance, though.
-  constexpr bool operator==(uint16_t raw) const {
-    return this->raw == raw;
-  }
-  constexpr bool operator>=(uint16_t raw) const {
-    return this->raw >= raw;
-  }
-  constexpr bool operator<=(uint16_t raw) const {
-    return this->raw <= raw;
-  }
-  constexpr bool operator>(uint16_t raw) const {
-    return this->raw > raw;
-  }
-  constexpr bool operator<(uint16_t raw) const {
-    return this->raw < raw;
   }
 
   // Comparison operators (Key objects)
@@ -105,20 +129,40 @@ union Key {
     return this->raw <= other.raw;
   }
 
+  // Comparison operators (integers). I don't think these are really necessary, or even
+  // desirable; we should remember to cast integers appropriately, instead. There's
+  // probably a small benefit in performance, though.
+  constexpr bool operator==(uint16_t val) const {
+    return this->raw == val;
+  }
+  constexpr bool operator>=(uint16_t val) const {
+    return this->raw >= val;
+  }
+  constexpr bool operator<=(uint16_t val) const {
+    return this->raw <= val;
+  }
+  constexpr bool operator>(uint16_t val) const {
+    return this->raw > val;
+  }
+  constexpr bool operator<(uint16_t val) const {
+    return this->raw < val;
+  }
+
   // Set key value from integer
-  inline Key& operator=(uint16_t raw) {
-    this->raw = raw;
+  inline Key& operator=(uint16_t val) {
+    this->raw = val;
     return *this;
   }
   // These two operators should be useful for decoding offsets
-  inline Key& operator+=(uint16_t raw) {
-    this->raw += raw;
+  inline Key& operator+=(uint16_t val) {
+    this->raw += val;
     return *this;
   }
-  inline Key& operator-=(uint16_t raw) {
-    this->raw -= raw;
+  inline Key& operator-=(uint16_t val) {
+    this->raw -= val;
     return *this;
   }
+  // Key object versions
   inline Key& operator+=(const Key& other) {
     this->raw += other.raw;
     return *this;
@@ -129,6 +173,13 @@ union Key {
   }
 
   // Other arithmetic operators for use with offsets
+  constexpr Key operator+(uint16_t val) const {
+    return Key(this->raw + val);
+  }
+  constexpr Key operator-(uint16_t val) const {
+    return Key(this->raw + val);
+  }
+  // Key object versions
   constexpr Key operator+(Key other) const {
     return Key(this->raw + other.raw);
   }
@@ -136,6 +187,40 @@ union Key {
     return Key(this->raw - other.raw);
   }
 
+  static constexpr byte keyboard_type = B000;
+  static constexpr byte consumer_type = B001000;
+  static constexpr byte   system_type = B00100100;
+  static constexpr byte    mouse_type = B00100101;
+  static constexpr byte    layer_type = B00100110;
+  static constexpr byte   plugin_type = B01;
+
+  // returns the correct type of the Key object
+  KeyClass type() {
+    if (keyboard.type == Key::keyboard_type)
+      return KeyClass::keyboard;
+    if (consumer.type == Key::consumer_type)
+      return KeyClass::consumer;
+    if (system.type == Key::system_type)
+      return KeyClass::system;
+    if (mouse.type == Key::mouse_type)
+      return KeyClass::mouse;
+    if (layer.type == Key::layer_type)
+      return KeyClass::layer;
+    if (plugin.type == Key::plugin_type)
+      return KeyClass::plugin;
+    return KeyClass::unknown;
+  }
+
+};
+
+enum class KeyClass : byte {
+  keyboard,
+  consumer,
+  system,
+  mouse,
+  layer,
+  plugin,
+  unknown
 };
 
 } // namespace model01 {
