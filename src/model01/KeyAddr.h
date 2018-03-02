850bef9fc1059e60f1478475ef7049cc9ad2e940
Michael Richters <gedankenexperimenter@gmail.com>
2018-03-02 15:00:48 -0600

    Minor adjustments to KeyAddr
    * removed unnecessary #include
    * set default value to 0xFF
    * removed commented helper functions

--- src/model01/KeyAddr.h
+++ src/model01/KeyAddr.h
@@ -3,8 +3,6 @@
 #pragma once
 
 #include <Arduino.h>
-#include <stdint.h>
-
 
 // Can this class be used as an iterator for range-based for loops? Layer classes would
 // need appropriate begin() & end() methods. See:
@@ -26,7 +24,7 @@ namespace model01 {
 // though it doesn't solve the problem I thought it would.
 struct KeyAddr {
 
-  byte addr;
+  byte addr {0xFF};
 
   // Default constructor initializes with an invalid addr
   KeyAddr() = default;
@@ -106,38 +104,6 @@ struct KeyAddr {
   // Maybe I should provide a cast operator to convert to LedAddr from KeyAddr?
 
 };
-
-#if 0
-namespace keyaddr {
-
-// Everything past this point should be unnecessary, but could possibly be helpful for
-// plugins that want to affect whole rows or columns. Note that this code defines 8 rows &
-// 8 columns, which is probably not what a user wants on a Model01 (the last four rows are
-// on the right half of the keyboard.
-
-// ROWS = 8
-// COLS = 8
-constexpr KeyAddr ROW_BITS = B00111000;
-constexpr KeyAddr COL_BITS = B00000111;
-
-// Conversion functions to help with back-compat and user addressing. I actually think
-// that these addresses are not great, since the thumb arc should be a separate row, and
-// the palm keys yet another. User addressing should maybe be this 3-tuple: {hand, row,
-// col}, but that could be handled with #defines, or the build scripts.
-constexpr byte row(KeyAddr key_addr) {
-  return (key_addr & ROW_BITS) >> 3;
-}
-
-constexpr byte col(KeyAddr key_addr) {
-  return (key_addr & COL_BITS);
-}
-
-constexpr KeyAddr addr(byte row, byte col) {
-  return ((row << 3) | col);
-}
-
-} // namespace keyaddr {
-#endif
 
 } // namespace model01 {
 } // namespace kaleidoscope {
