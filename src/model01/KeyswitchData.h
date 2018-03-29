8d4c6f9f8ed47a8d8be557284205dbbe8d1ee35a
Michael Richters <gedankenexperimenter@gmail.com>
2018-03-29 15:46:39 -0500

    Changed KeyswitchData from union to struct

--- src/model01/KeyswitchData.h
+++ src/model01/KeyswitchData.h
@@ -9,9 +9,8 @@ namespace kaleidoscope {
 namespace hardware {
 
 // As with "Key", this name should be changed to make it clear what's represented
-union KeyswitchData {
+struct KeyswitchData {
   byte banks[4]; // 32 keys per scanner / 8 bits per byte
-  uint32_t all;
 };
 
 } // namespace hardware {
