636ab08bd1d4b07c4fba95af93f0c10ba8181e93
Michael Richters <gedankenexperimenter@gmail.com>
2019-02-27 11:19:23 -0600

    Clean up key_led_map
    Put it in an appropriate namespace, give it a permanent name, clean up some comments, and
    remove the original, non-functional version.
    
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- src/model01/LedAddr.cpp
+++ src/model01/LedAddr.cpp
@@ -4,9 +4,10 @@
 
 namespace kaleidoglyph {
 
+namespace hardware {
 // Translate from KeyAddr to LedAddr by using this table. The index in the array is the
 // KeyAddr; the value is the LedAddr.
-static constexpr PROGMEM byte key_led_map2[] = {
+static constexpr PROGMEM byte key_led_map[] = {
   27, 26, 20, 19, 12, 11,  4,  3,
   28, 25, 21, 18, 13, 10,  5,  2,
   29, 24, 22, 17, 14,  9,  6,  1,
@@ -17,13 +18,10 @@ static constexpr PROGMEM byte key_led_map2[] = {
   62, 57, 54, 49, 46, 41, 39, 34,
   63, 56, 55, 48, 47, 40, 32, 33,
 };
+} // namespace hardware {
 
 LedAddr::LedAddr(KeyAddr k) {
-  addr_ = pgm_read_byte( &(key_led_map2[byte(k)]) );
+  addr_ = pgm_read_byte( &(hardware::key_led_map[byte(k)]) );
 }
-
-// inline LedAddr getProgmemLedAddr(const LedAddr& pgm_led_addr) {
-//   return LedAddr(pgm_read_byte(&pgm_led_addr));
-// }
 
 } // namespace kaleidoglyph {
--- src/model01/LedAddr.h
+++ src/model01/LedAddr.h
@@ -14,20 +14,6 @@ class LedAddr {
  private:
   byte addr_;
 
-  // Translate from KeyAddr to LedAddr by using this table. The index in the array is the
-  // KeyAddr; the value is the LedAddr.
-  static constexpr PROGMEM byte key_led_map[] = {
-    27, 26, 20, 19, 12, 11,  4,  3,
-    28, 25, 21, 18, 13, 10,  5,  2,
-    29, 24, 22, 17, 14,  9,  6,  1,
-    30, 31, 23, 16, 15,  8,  7,  0,
-
-    60, 59, 52, 51, 44, 43, 37, 36,
-    61, 58, 53, 50, 45, 42, 38, 35,
-    62, 57, 54, 49, 46, 41, 39, 34,
-    63, 56, 55, 48, 47, 40, 32, 33,
-  };
-
  public:
   LedAddr() = default;
 
